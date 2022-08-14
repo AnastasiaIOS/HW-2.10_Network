2d7f923ff01f2d269b374c13360c11751be836f1
Anastasia <pavlova.a.s@inbox.ru>
2022-08-15 00:54:14 +0300

    aff

---
+++ HW 2.10_Network/MainViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MainViewController.swift
+//  HW 2.10_Network
+//
+//  Created by Anastasia on 27.07.2022.
+//
+
+import UIKit
+
+let link = ""
+
+private let reuseIdentifier = "Cell"
+
+class MainViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       
+        1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell // создается объект ячейки и кастим до типа нашего класса
+     
+    
+        
+    
+        return cell
+    }
+}
+    // подписываем под протокол, чтобы сделать ячейку динамической
+    extension MainViewController: UICollectionViewDelegateFlowLayout {
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
+        }
+        
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+
---
+++ HW 2.10_Network/UserActionCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserActionCell.swift
+//  HW 2.10_Network
+//
+//  Created by Anastasia on 14.08.2022.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var userLabel: UILabel!
+    
+}
