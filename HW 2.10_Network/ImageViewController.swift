9b0f3c9451b5d780ccc13c9032a5163d89c64b49
Anastasia <pavlova.a.s@inbox.ru>
2022-08-18 23:57:46 +0300

    add

--- HW 2.10_Network/ImageViewController.swift
+++ HW 2.10_Network/ImageViewController.swift
@@ -15,7 +15,7 @@ class ImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.startAnimating()
     }
 
 
