efb7303a4dc76bbae3876719bcba5ef5f1d52e68
Anastasia <pavlova.a.s@inbox.ru>
2022-08-20 12:03:17 +0300

    add

---
+++ HW 2.10_Network/Owner(Model).swift
@@ -0,0 +1,14 @@
+//
+//  Owner(Model).swift
+//  HW 2.10_Network
+//
+//  Created by Anastasia on 20.08.2022.
+//
+
+
+struct Owner {
+    let name: String
+    let age: Int
+    let count: Int
+
+}
