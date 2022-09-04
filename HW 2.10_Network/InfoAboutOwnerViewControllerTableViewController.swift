f25fdd9f222e3a0f229a454ce54a060f7f77aa47
Anastasia <pavlova.a.s@inbox.ru>
2022-09-04 22:47:16 +0300

    add

--- HW 2.10_Network/InfoAboutOwnerViewControllerTableViewController.swift
+++ HW 2.10_Network/InfoAboutOwnerViewControllerTableViewController.swift
@@ -28,6 +28,7 @@ class InfoAboutOwnerViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
     3
+        
     }
     
     
@@ -36,7 +37,7 @@ class InfoAboutOwnerViewController: UITableViewController {
         
         var content = cell.defaultContentConfiguration()
         let owner = owners[indexPath.row]
-        //????????????
+        //???????????? ВОТ ТУТ ДАЛЬШЕ НЕ ЗНАЮ КАК СДЕЛАТЬ ТАК, ЧТОБЫ В КАЖДУЮ СТРОКУ РАЗМЕСТИТЬ СВОЙСТВО МОДЕЛИ((
         
         return cell
     }
@@ -70,50 +71,3 @@ extension InfoAboutOwnerViewController {
 }
 
  
-
-/*
- // Override to support conditional editing of the table view.
- override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the specified item to be editable.
- return true
- }
- */
-
-/*
- // Override to support editing the table view.
- override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
- if editingStyle == .delete {
- // Delete the row from the data source
- tableView.deleteRows(at: [indexPath], with: .fade)
- } else if editingStyle == .insert {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
- 
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the item to be re-orderable.
- return true
- }
- */
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
-
